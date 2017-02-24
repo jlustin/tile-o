ef24aea91a28123c4c578dff22507ddc7acdfc0c
JunYu Lei <junyuu@hotmail.com>
2017-02-24 14:56:45 -0500

    Draft TileOApplication

---
+++ TileOPart3/src/ca/mcgill/ecse223/tileo/application/TileOApplication.java
@@ -0,0 +1,54 @@
+package ca.mcgill.ecse223.tileo.application;
+
+import ca.mcgill.ecse223.tileo.model.TileO;
+import ca.mcgill.ecse223.tileo.model.Game;
+import ca.mcgill.ecse223.tileo.model.Player;
+
+public class TileOApplication {
+	private static TileO tileO;
+	private static String filename = "data.tileO";
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// start UI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TileOPage().setVisible(true);
+            }
+        });
+        
+	}
+
+	public static TileO getTileO() {
+		if (tileO == null) {
+			// load model
+			tileO = load();
+		}
+ 		return tileO;
+	}
+	
+	public static void save() {
+		PersistenceObjectStream.serialize(tileO);
+	}
+	
+	public static TileO load() {
+		PersistenceObjectStream.setFilename(filename);
+		tileO = (TileO) PersistenceObjectStream.deserialize();
+		// model cannot be loaded - create empty BTMS
+		if (tileO == null) {
+			tileO = new TileO();
+			Game game = new Game (32, tileO);
+			tileO.setCurrentGame(game);
+		}
+		else {
+			Player.reinitializeUniquePlayer();
+		}
+		return tileO;
+	}
+	
+	public static void setFilename(String newFilename) {
+		filename = newFilename;
+	}
+}
