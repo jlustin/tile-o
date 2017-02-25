74f3d399cf10c7687df9b9b95ff72f823b75f43f
JunYu Lei <junyuu@hotmail.com>
2017-02-24 19:02:52 -0500

    Update

--- TileOPart3/src/ca/mcgill/ecse223/tileo/controller/PlayModeController.java
+++ TileOPart3/src/ca/mcgill/ecse223/tileo/controller/PlayModeController.java
@@ -1,29 +1,39 @@
 package ca.mcgill.ecse223.tileo.controller;
 
+import ca.mcgill.ecse223.tileo.application.TileOApplication;
+import ca.mcgill.ecse223.tileo.model.ActionCard;
+import ca.mcgill.ecse223.tileo.model.ConnectTilesActionCard;
+import ca.mcgill.ecse223.tileo.model.Deck;
+import ca.mcgill.ecse223.tileo.model.Game;
 import ca.mcgill.ecse223.tileo.model.Tile;
+import ca.mcgill.ecse223.tileo.model.TileO;
 
 public class PlayModeController {
 
 	// METHODS
 	
-	public void actionCardAddConnection (Tile selectedTile1, Tile selectedTile2){
-		// Get tileO from TilOApplication
-		// Get current game from tileO
-		// Get currentConnectionPieces from game
+	public void playConnectTilesActionCard (Tile selectedTile1, Tile selectedTile2){
+		TileO tileO = TileOApplication.getTileO();
+		Game currentGame = tileO.getCurrentGame();
+		Deck deck = currentGame.getDeck();
+		ActionCard currentCard = deck.getCard(0);
 		
-		int usedConnections = game.getCurrentConnectionPieces();
+		ConnectTilesActionCard.play(selectedTile1, selectedTile2);
 		
-		try 
-		{
-			if (usedConnections <= 32){
-				newConnection =  game.addConnection;
-				selectedTile1.addConnection(newConnection);
-				selectedTile2.addConnection(newConnection);
-			}
-		}
-		catch (RuntimeException e){
-			throw new InvalidInputException (e.getMessage());
-		}
+		
+//		int usedConnections = currentGame.getCurrentConnectionPieces();
+//		
+//		try 
+//		{
+//			if (usedConnections <= 32){
+//				newConnection =  game.addConnection;
+//				selectedTile1.addConnection(newConnection);
+//				selectedTile2.addConnection(newConnection);
+//			}
+//		}
+//		catch (RuntimeException e){
+//			throw new InvalidInputException (e.getMessage());
+//		}
 	}
 	
 }
