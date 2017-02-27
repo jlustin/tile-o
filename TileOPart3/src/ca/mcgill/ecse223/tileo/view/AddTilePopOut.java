7eb56c6b54699c7e416c9e47efad126e44eb320a
berri3 <cmliang2004@hotmail.com>
2017-02-27 15:51:18 -0500

    Grouped the RadioButtons in AddTilePopOut
    Using WindowBuilder

--- TileOPart3/src/ca/mcgill/ecse223/tileo/view/AddTilePopOut.java
+++ TileOPart3/src/ca/mcgill/ecse223/tileo/view/AddTilePopOut.java
@@ -17,12 +17,14 @@ import javax.swing.JRadioButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.ButtonGroup;
 
 public class AddTilePopOut extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -53,7 +55,7 @@ public class AddTilePopOut extends JFrame {
 	
 	public AddTilePopOut() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 468, 374);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -68,10 +70,14 @@ public class AddTilePopOut extends JFrame {
 		});
 		
 		JRadioButton rdbtnNormalTile = new JRadioButton("Normal Tile");
+		buttonGroup.add(rdbtnNormalTile);
 		
 		JRadioButton rdbtnActionTile = new JRadioButton("Action Tile");
+		rdbtnActionTile.setHorizontalAlignment(SwingConstants.CENTER);
+		buttonGroup.add(rdbtnActionTile);
 		
 		JRadioButton rdbtnWinTile = new JRadioButton("Win Tile");
+		buttonGroup.add(rdbtnWinTile);
 		
 		JLabel lblNewLabel = new JLabel("x component:");
 		
@@ -86,35 +92,33 @@ public class AddTilePopOut extends JFrame {
 		JLabel lblAddANew = new JLabel("Add a new tile to the board game!");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(126)
 					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 178, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(136, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(rdbtnNormalTile)
-					.addGap(59)
-					.addComponent(rdbtnActionTile)
-					.addPreferredGap(ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
-					.addComponent(rdbtnWinTile)
-					.addGap(21))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel)
-					.addGap(57)
-					.addComponent(lblYComponent)
-					.addContainerGap(204, Short.MAX_VALUE))
+					.addContainerGap(114, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(162, Short.MAX_VALUE))
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap(269, Short.MAX_VALUE)
+					.addContainerGap(96, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(66, Short.MAX_VALUE)
 					.addComponent(lblAddANew)
 					.addGap(110))
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblNewLabel)
+						.addComponent(rdbtnNormalTile))
+					.addGap(33)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblYComponent)
+						.addComponent(rdbtnActionTile))
+					.addPreferredGap(ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+					.addComponent(rdbtnWinTile)
+					.addGap(21))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
@@ -134,7 +138,7 @@ public class AddTilePopOut extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
