3726b712a52d4f08ddbcd20da27508354e98eb99
LiTigre <li.zhang17@mail.mcgill.ca>
2017-02-27 20:15:57 -0500

    tilePanel

--- TileOPart3/src/ca/mcgill/ecse223/tileo/view/TileODesignPage.java
+++ TileOPart3/src/ca/mcgill/ecse223/tileo/view/TileODesignPage.java
@@ -10,9 +10,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.border.EmptyBorder;
+
+import ca.mcgill.ecse223.tileo.application.TileOApplication;
+
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JRadioButton;
+import javax.swing.JSplitPane;
 
 public class TileODesignPage extends JFrame {
 
@@ -21,18 +25,18 @@ public class TileODesignPage extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					TileODesignPage frame = new TileODesignPage();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TileODesignPage frame = new TileODesignPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the frame.
@@ -48,19 +52,18 @@ public class TileODesignPage extends JFrame {
 	}
 	
 	private void initComponents(){
-								
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//this.setBounds(100, 100, 450, 300);
+		this.setSize(1200, 700);
+		this.setResizable(false);
+		
 		contentPane = new JPanel();
-		setSize(1200, 700);
-		setResizable(false);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		
-		
-		
-		
-		
+		contentPane.setSize(500, 700);
+		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setVisible(true);
+		//setContentPane(contentPane);
+				
 		JButton btnOpenNewWindow = new JButton("Add Tiles");
 		btnOpenNewWindow.addActionListener(new ActionListener() {
 			
@@ -89,26 +92,16 @@ public class TileODesignPage extends JFrame {
 		});
 		
 		JButton btnRemoveConnections = new JButton("Remove Connections");
-		
-		JButton btnCreateDeck = new JButton("Add Deck");
-		btnCreateDeck.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				DeckPopOut d = new DeckPopOut();
-				d.setVisible(true);
-			}
-		});
-		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(1008, Short.MAX_VALUE)
+					.addContainerGap(284, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addComponent(btnOpenNewWindow, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnRemoveTiles, Alignment.TRAILING)
 						.addComponent(btnAddConnections, Alignment.TRAILING)
-						.addComponent(btnRemoveConnections, Alignment.TRAILING)
-						.addComponent(btnCreateDeck, Alignment.TRAILING))
+						.addComponent(btnRemoveConnections, Alignment.TRAILING))
 					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
@@ -122,14 +115,30 @@ public class TileODesignPage extends JFrame {
 					.addComponent(btnAddConnections)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(btnRemoveConnections)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnCreateDeck)
-					.addContainerGap(458, Short.MAX_VALUE))
+					.addContainerGap(128, Short.MAX_VALUE))
 		);
-		contentPane.setLayout(gl_contentPane);
+		//contentPane.setLayout(gl_contentPane);
+		
+		
+		TilePanel grid = new TilePanel(TileOApplication.getTileO().getCurrentGame());
+		grid.setVisible(true);
+		grid.setSize(700, 700);
+		
+		
+		
+		
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setSize(1200, 700);
+	    splitPane.setDividerSize(0);
+	    splitPane.setDividerLocation(700);
+	    splitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+	    splitPane.setLeftComponent(grid);
+	    splitPane.setRightComponent(contentPane);
+
+	    this.add(splitPane);
 	}
 		
-
 	private void refreshData(){
 		
 	}
--- TileOPart3/src/ca/mcgill/ecse223/tileo/view/TilePanel.java
+++ TileOPart3/src/ca/mcgill/ecse223/tileo/view/TilePanel.java
@@ -8,59 +8,188 @@ import ca.mcgill.ecse223.tileo.model.NormalTile;
 import ca.mcgill.ecse223.tileo.model.Tile;
 import ca.mcgill.ecse223.tileo.model.TileO;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Rectangle;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 
 
 public class TilePanel extends JPanel{
 
-	/**
-	 * Create the panel.
-	 */
-	public TilePanel() {
-		TileO tileO = TileOApplication.getTileO();
-		Game currentGame = tileO.getCurrentGame();
-//		Tile e = new NormalTile(1, 2, currentGame);
-//		new NormalTile(1, 1, currentGame);
-//		new NormalTile(5, 5, currentGame);
-//		
-		List<Tile> listTiles = currentGame.getTiles();
-		//listTiles.add(e);
+	//UI elements
+	private List<Rectangle2D> rectangles = new ArrayList<Rectangle2D>();
+	private static final int MINWIDTH = 9;
+	private static final int MINHEIGHT = 9;
+	
+	
+	// data element;
+	private Game game;
+	private HashMap<Rectangle2D, Tile> tiles;
+	private Tile selectedTile;
+	
+	
+	
+	
+	public TilePanel(Game game) {
+		init(game);
 		
-		int i = getXAxis(currentGame);
-		int j = getYAxis(currentGame);
-		//TODO: set default size and bigger size
-		if (i<6) {
-			i=6;
+		new NormalTile(1, 2, game);
+		new NormalTile(1, 1, game);
+		new NormalTile(5, 5, game);
+		new NormalTile(8, 15, game);
+		
+		List<Tile> listTiles = game.getTiles();
+		
+		int i = getXAxis(game);
+		int j = getYAxis(game);
+
+		if (i<MINWIDTH) {
+			i=MINWIDTH;
+			
 		}
-		if (j<6) {
-			j=6;
+		
+		if(j<MINHEIGHT) {
+			j=MINHEIGHT;
+		}
+		
+		if(i>j) {
+			j=i;
+		}
+		if(j>i) {
+			i=j;
 		}
 		
 		JPanel[][] panelHolder = new JPanel[i][j];
-		setLayout(new GridLayout(i, j, 2, 2));
-		for(int m = 0; m < i; m++) {
-		   for(int n = 0; n < j; n++) {
-		      panelHolder[m][n] = new JPanel();
-		      add(panelHolder[m][n]);
-		      }
-		   }
-		 
-		for (Tile aTile: listTiles) {
-			 
-			panelHolder[aTile.getX()][aTile.getY()].add(new JButton("Foo") );
-			
-			
-		}
+		GridLayout grid = new GridLayout(i, j, 300/i, 300/j);
+		//grid.setHgap(i/6);
+		//grid.setVgap(j/6);
+		setLayout(grid);
 		
+		for(int m = 0; m < i; m++) {
+			for(int n = 0; n < j; n++) {
+				panelHolder[m][n] = new JPanel();
+				add(panelHolder[m][n]);
+				//panelHolder[m][n].setBorder(BorderFactory.createLineBorder(Color.black));
+				//panelHolder[m][n].setOpaque(true);
+			}
+		}
+		for (Tile aTile: listTiles) {
+			
+			int x = aTile.getX()-1;
+			int y = aTile.getY()-1;
+			//RectDraw newrect = new RectDraw();
+			panelHolder[x][y].setBorder(BorderFactory.createLineBorder(Color.black));
+			//panelHolder[x][y].
+		}
+			
+		
+	}
+	
+	
+	private void init(Game game) {
+		this.game = game;
+		tiles = new HashMap<Rectangle2D, Tile>();
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				int x = e.getX();
+				int y = e.getY();
+				for (Rectangle2D rectangle : rectangles) {
+					if (rectangle.contains(x, y)) {
+						selectedTile = tiles.get(rectangle);
+						break;
+					}
+				}
+				repaint();
+			}
+		});		
+	}
+	
+	
+	
+	public void createTile(JPanel panel) {
+		float w = panel.getWidth();
+		float h = panel.getHeight();
+		Rectangle2D rect = new Rectangle2D.Float(0, 0, w, h);
 		
 	}
 
+	
+	/*
+	public void doDrawing(Graphics g) {
+		if (game != null) {
+			TileO tileO = TileOApplication.getTileO();
+			Game currentGame = tileO.getCurrentGame();
+			new NormalTile(1, 2, currentGame);
+			new NormalTile(1, 1, currentGame);
+			new NormalTile(5, 5, currentGame);
+			
+			List<Tile> listTiles = currentGame.getTiles();
+			
+			int i = getXAxis(currentGame);
+			int j = getYAxis(currentGame);
+
+			if (i<MINWIDTH) {
+				i=MINWIDTH;
+			}
+			if (j<MINHEIGHT) {
+				j=MINHEIGHT;
+			}
+			
+			JPanel[][] panelHolder = new JPanel[i][j];
+			GridLayout grid = new GridLayout(i*2 -1, j*2 -1);
+			//grid.setHgap(i/6);
+			//grid.setVgap(j/6);
+			setLayout(grid);
+			
+			for(int m = 0; m < i; m++) {
+				for(int n = 0; n < j; n++) {
+					panelHolder[m][n] = new JPanel();
+					add(panelHolder[m][n]);
+				}
+			}
+			for (Tile aTile: listTiles) {
+				int x = aTile.getX();
+				int y = aTile.getY();
+				panelHolder[x][y].add(newrect);
+				
+			}
+				
+		}
+	}
+	
+	*/
+	
+	private static class RectDraw extends JPanel {
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public void paintComponent(Graphics g) {
+        super.paintComponent(g);  
+         g.drawRect(230,80,10,10);  
+         g.setColor(Color.RED);  
+         g.fillRect(230,80,10,10);  
+         }
+
+	}		
+		
+	
+/*
+	public Rectangle2D drawTile(JPanel panelHolder) {
+		int h = panelHolder.getHeight();
+		int w = panelHolder.getWidth();
+		Rectangle2D rectangle = new Rectangle2D.Float(h*(1/6), w*(1/6), w*(2/3), h*(2/3));
+		return rectangle;
+	}
+
+*/	
 	
 	public int getXAxis(Game aGame) {
 		List<Tile> listTiles = aGame.getTiles();
@@ -87,5 +216,13 @@ public class TilePanel extends JPanel{
 		
 		return ySize;		
 	}
+	
+	
 
 }
+
+
+
+
+
+
