09bb0e7e58efcf3226f96734bc441c008b08d64f
thechrizilla <chris.chan@mail.mcgill.ca>
2017-02-28 18:37:38 -0500

    changed some stuff in SetPlayerStartingTilePopOut

--- TileOPart3/src/ca/mcgill/ecse223/tileo/view/SetPlayerStartingTIlePopOut.java
+++ TileOPart3/src/ca/mcgill/ecse223/tileo/view/SetPlayerStartingTIlePopOut.java
@@ -40,18 +40,18 @@ public class SetPlayerStartingTIlePopOut extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					SetPlayerStartingTIlePopOut frame = new SetPlayerStartingTIlePopOut();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SetPlayerStartingTIlePopOut frame = new SetPlayerStartingTIlePopOut();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the frame.
@@ -71,12 +71,20 @@ public class SetPlayerStartingTIlePopOut extends JFrame {
 		JLabel lblSelectTheNumber = new JLabel("Select the player's number and");
 		
 		JRadioButton radioButton = new JRadioButton("2");
+		radioButton.setActionCommand("2");
+		buttonGroup.add(radioButton);
 		
 		JRadioButton radioButton_1 = new JRadioButton("3");
+		radioButton_1.setActionCommand("3");
+		buttonGroup.add(radioButton_1);
 		
 		JRadioButton radioButton_2 = new JRadioButton("4");
+		radioButton_2.setActionCommand("4");
+		buttonGroup.add(radioButton_2);
 		
 		JRadioButton radioButton_3 = new JRadioButton("1");
+		radioButton_3.setActionCommand("1");
+		buttonGroup.add(radioButton_3);
 		
 		JLabel lblChooseItsStarting = new JLabel("choose its starting tile from the board.");
 		
@@ -84,22 +92,17 @@ public class SetPlayerStartingTIlePopOut extends JFrame {
 		btnSet.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				error = "";
+//				error = "";
 				chosenNumber = buttonGroup.getSelection();
 				playerNumber = chosenNumber.getActionCommand();
 				chosenPlayerNumber = Integer.parseInt(playerNumber);
 				
 				
-				try{
-					DesignModeController dmc = new DesignModeController();
-					Player currentPlayer = TileO.getCurrentGame().getPlayer(chosenPlayerNumber-1);
-					NormalTile startingTile = new NormalTile(0,0,null);
-					dmc.setPlayerStartingTile(currentPlayer, startingTile);
-					
-				}catch (InvalidInputException ee){
-					error = ee.getMessage();
-					
-				}
+				System.out.println(chosenPlayerNumber - 1);
+//				DesignModeController dmc = new DesignModeController();
+//				Player currentPlayer = TileO.getCurrentGame().getPlayer(chosenPlayerNumber-1);
+//				NormalTile startingTile = new NormalTile(0,0,null);
+//				dmc.setPlayerStartingTile(currentPlayer, startingTile);
 				close();
 			}
 		});
