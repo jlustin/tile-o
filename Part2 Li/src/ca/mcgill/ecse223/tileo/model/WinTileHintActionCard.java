00239de4f3d0bc5f945f055cb6dbf2a4725058b0
JunYu Lei <junyuu@hotmail.com>
2017-04-04 20:34:39 -0400

    WinTileHint Implementation

--- Part2 Li/src/ca/mcgill/ecse223/tileo/model/WinTileHintActionCard.java
+++ Part2 Li/src/ca/mcgill/ecse223/tileo/model/WinTileHintActionCard.java
@@ -4,6 +4,8 @@
 package ca.mcgill.ecse223.tileo.model;
 import java.io.Serializable;
 
+import ca.mcgill.ecse223.tileo.controller.InvalidInputException;
+
 // line 128 "../../../../../TileOPersistence.ump"
 // line 416 "../../../../../TileO (updated April3).ump"
 public class WinTileHintActionCard extends ActionCard
@@ -42,8 +44,64 @@ public class WinTileHintActionCard extends ActionCard
   }
 
   // line 426 "../../../../../TileO (updated April3).ump"
-   public void play(Tile aTile){
-    
+   public boolean play(Tile aTile) throws InvalidInputException {
+	   	Game currentGame = super.getDeck().getGame();
+	    boolean hint = false;
+	    
+	    Tile topTile;
+	    Tile bottomTile;
+	    Tile rightTile;
+	    Tile leftTile;
+	    
+	    String error = "";
+
+			for (Tile tileInGame : currentGame.getTiles()){
+				if (aTile.getY() == tileInGame.getY() -1){
+					topTile = tileInGame;
+				}
+				if (aTile.getY() == tileInGame.getY() +1){
+					bottomTile = tileInGame;
+				}
+				if (aTile.getX() == tileInGame.getX() -1){
+					leftTile = tileInGame;
+				}
+				if (aTile.getX() == tileInGame.getY() +1){
+					rightTile = tileInGame;
+				}
+			}
+	    
+			try {
+				if (aTile instanceof WinTile) {
+					hint  = true;
+				}
+				if (topTile != null){
+					if (topTile instanceof WinTile) {
+						hint = true;
+					}
+				}
+				if (bottomTile != null) {
+					
+					if (bottomTile instanceof WinTile) {
+						hint = true;
+					}
+				}
+				if (rightTile != null) {
+					if (rightTile instanceof WinTile) {
+						hint = true;
+					}				
+				}
+				if (leftTile != null) {
+					if (leftTile instanceof WinTile) {
+						hint = true;
+					}
+				}
+				return hint;
+			}
+			catch (RuntimeException e) {
+				error = e.getMessage();
+				throw new InvalidInputException(error);
+			}
+	  }
   }
   
   //------------------------
