f5b80938570ececc83172096f2f6f030a422a5cc
thechrizilla <chris.chan@mail.mcgill.ca>
2017-04-05 19:46:53 -0400

    changed SendBackToStartPopOut

--- Part2 Li/src/ca/mcgill/ecse223/tileo/view/SendBackToStartPopOut.java
+++ Part2 Li/src/ca/mcgill/ecse223/tileo/view/SendBackToStartPopOut.java
@@ -14,156 +14,89 @@ import ca.mcgill.ecse223.tileo.model.Game;
 import ca.mcgill.ecse223.tileo.model.Player;
 import ca.mcgill.ecse223.tileo.model.TileO;
 
-import javax.swing.ButtonGroup;
 import javax.swing.ButtonModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JRadioButton;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class SendBackToStartPopOut extends JFrame {
-	private PlayController pmc = TileOPlayPage.pmc;
-	private int chosenPlayer;
-	private String error;
 
 	private JPanel contentPane;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
+	public static int numberOfPlayers;
+
+		
 
 
 	/**
 	 * Create the frame.
 	 */
 	public SendBackToStartPopOut() {
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		String error;
 		
-		JLabel lblYouHaveDrawn = new JLabel("You have drawn a Send Back to Start Acton Card,");
+		JLabel lblYouHaveDrawn = new JLabel("You have drawn a Send Back To Start Action Card, please select");
 		
-		JLabel lblNewLabel = new JLabel("please select a player");
+		JLabel lblAPleyerTo = new JLabel("a player and click \"Send\" button to send");
 		
-		JLabel lblAndClickOn = new JLabel("and click on the \"Send\" button to send him/her");
+		JLabel lblThatPlayerBack = new JLabel("that player back to its starting position.");
 		
-		JLabel lblBack = new JLabel("back to his/her starting position.");
+		JComboBox comboBox_1 = new JComboBox();
 		
-		JRadioButton radioButton = new JRadioButton("1");
-		buttonGroup.add(radioButton);
 		
-		JRadioButton radioButton_1 = new JRadioButton("2");
-		buttonGroup.add(radioButton_1);
 		
-		JRadioButton radioButton_2 = new JRadioButton("3");
-		buttonGroup.add(radioButton_2);
-		
-		JRadioButton radioButton_3 = new JRadioButton("4");
-		buttonGroup.add(radioButton_3);
+		String[] playerNum = new String[]
 		
 		JButton btnSend = new JButton("Send");
 		btnSend.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				error = "";
-				TileO tileO = TileOApplication.getTileO();
-				Game currentGame = tileO.getCurrentGame();
-				Player currentPlayer = currentGame.getCurrentPlayer();
-				int number = getWithNumber(currentPlayer);
-
-				ButtonModel chosenPlayer = buttonGroup.getSelection();
-				
-				if(chosenPlayer.getActionCommand() == null){
-					error = error + "Please select a player";
-				}
-				
-				
-				if(Integer.parseInt(chosenPlayer.getActionCommand()) == number){
-					error = error + "You cannot send yourself back to your starting position!";
-					
-				}
 				
 				
 				
-				error.trim();
-				if(error.length()==0){
-					try{
-						PlayController pmc = new PlayController();
-						int index = Integer.parseInt(chosenPlayer.getActionCommand());
-						pmc.playSendBackToStartActionCard(index+1);
-					}
-					catch(InvalidInputException e1){
-						throw new RuntimeException(e1.getMessage());
-					}
-				}
 				
-				
-				
-			}
-
-			private int getWithNumber(Player currentPlayer) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			private String parsInt(String actionCommand) {
-				// TODO Auto-generated method stub
-				return null;
 			}
 		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(69)
-							.addComponent(lblYouHaveDrawn))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(145)
-							.addComponent(lblNewLabel))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(113)
-							.addComponent(lblBack))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(80)
-							.addComponent(lblAndClickOn))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(189)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-								.addComponent(radioButton_1)
-								.addComponent(radioButton)
-								.addComponent(radioButton_3)
-								.addComponent(radioButton_2)))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(174)
-							.addComponent(btnSend)))
-					.addContainerGap(63, Short.MAX_VALUE))
+					.addGap(26)
+					.addComponent(lblYouHaveDrawn))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(96)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblThatPlayerBack)
+						.addComponent(lblAPleyerTo)))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(185)
+					.addComponent(btnSend))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(196, Short.MAX_VALUE)
+					.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(192))
 		);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(18)
+					.addGap(19)
 					.addComponent(lblYouHaveDrawn)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblNewLabel)
+					.addComponent(lblAPleyerTo)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblAndClickOn)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblBack)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(radioButton)
-					.addGap(3)
-					.addComponent(radioButton_1)
-					.addGap(5)
-					.addComponent(radioButton_2)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(radioButton_3)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblThatPlayerBack)
+					.addPreferredGap(ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
+					.addComponent(comboBox_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(45)
 					.addComponent(btnSend)
-					.addContainerGap(15, Short.MAX_VALUE))
+					.addGap(31))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
