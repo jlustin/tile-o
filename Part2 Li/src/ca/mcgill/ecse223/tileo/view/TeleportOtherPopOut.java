a78ec326c643e9bbfe9f39df68d245025a0825d5
xd00258 <victoriquevuong@gmail.com>
2017-04-05 20:07:55 -0400

    fixed order of arguments

---
+++ Part2 Li/src/ca/mcgill/ecse223/tileo/view/TeleportOtherPopOut.java
@@ -0,0 +1,42 @@
+package ca.mcgill.ecse223.tileo.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TeleportOtherPopOut extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TeleportOtherPopOut frame = new TeleportOtherPopOut();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TeleportOtherPopOut() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
