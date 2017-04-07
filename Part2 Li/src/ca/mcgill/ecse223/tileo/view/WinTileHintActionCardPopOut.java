b7525dc54c9f7992e926c595cb654defbf3fc568
JunYu Lei <junyuu@hotmail.com>
2017-04-06 20:21:01 -0400

    wintileAC neighbour definition update

--- Part2 Li/src/ca/mcgill/ecse223/tileo/view/WinTileHintActionCardPopOut.java
+++ Part2 Li/src/ca/mcgill/ecse223/tileo/view/WinTileHintActionCardPopOut.java
@@ -60,7 +60,7 @@ public class WinTileHintActionCardPopOut extends JFrame {
 		setAlwaysOnTop(true);
 		setTitle("Win Tile Hint Action Card");
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		setBounds(500, 200, 450, 300);
+		setBounds(500, 200, 550, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -137,6 +137,10 @@ public class WinTileHintActionCardPopOut extends JFrame {
 		errorLabel = new JLabel("");
 		errorLabel.setForeground(Color.RED);
 		
+		JLabel lbltwoTilesAre = new JLabel("*Two tiles are neighbours if they are next to each other vertically or horizontally, ");
+		
+		JLabel lblTheyAreNot = new JLabel("but they are not necessarily connected!");
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -145,7 +149,7 @@ public class WinTileHintActionCardPopOut extends JFrame {
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(74)
 							.addComponent(getHintButton)
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
 							.addComponent(willRemButton)
 							.addGap(94))
 						.addGroup(gl_contentPane.createSequentialGroup()
@@ -154,15 +158,23 @@ public class WinTileHintActionCardPopOut extends JFrame {
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(110)
 							.addComponent(errorLabel, GroupLayout.PREFERRED_SIZE, 209, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(44, Short.MAX_VALUE))
+					.addContainerGap(51, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(25)
 					.addComponent(selectTileLabel)
-					.addContainerGap(49, Short.MAX_VALUE))
+					.addContainerGap(24, Short.MAX_VALUE))
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(82)
 					.addComponent(drawnCardLabel)
 					.addContainerGap(120, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lbltwoTilesAre)
+					.addContainerGap(21, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblTheyAreNot, GroupLayout.PREFERRED_SIZE, 382, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(32, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -175,11 +187,15 @@ public class WinTileHintActionCardPopOut extends JFrame {
 					.addComponent(errorLabel)
 					.addGap(18)
 					.addComponent(hintLabel)
-					.addPreferredGap(ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(getHintButton)
 						.addComponent(willRemButton))
-					.addGap(64))
+					.addGap(26)
+					.addComponent(lbltwoTilesAre)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblTheyAreNot)
+					.addContainerGap())
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
